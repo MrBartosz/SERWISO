52904fbec0384644f20624aa990b501a863c2340
MrBartosz <maksimowski97@gmail.com>
2025-07-14 19:24:38 +0200

    Inicjalizacja Projektu

---
+++ frontend/src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-background text-foreground">
+      <h1 className="text-3xl font-bold">Witaj w Serwiso!</h1>
+    </div>
+  );
+}
